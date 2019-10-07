9bc80313b3f0521da2c7cdd0246824dbd37a1918
Junil Ko <goyadb@naver.com>
2019-10-07 18:20:45 +0900

    스위치 구현 중

--- Assets/Scripts/Menu/MenuCtrl.cs
+++ Assets/Scripts/Menu/MenuCtrl.cs
@@ -5,8 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MenuCtrl : MonoBehaviour
 {
+    public GameObject settingsPanel;
+
+    private void Start()
+    {
+        settingsPanel.SetActive(false);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    public void OpenSettings()
+    {
+        // 설정창 열기
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        // 설정창 닫기
+        settingsPanel.SetActive(false);
     }
 }
