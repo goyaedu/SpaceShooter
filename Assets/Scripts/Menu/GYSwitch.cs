c552a9b14a913f0e5cf4096538d9e9d07b9714a2
Junil Ko <goyadb@naver.com>
2019-10-14 11:11:47 +0900

    p.494까지 진행함

--- Assets/Scripts/Menu/GYSwitch.cs
+++ Assets/Scripts/Menu/GYSwitch.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GYSwitch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public RectTransform handleRectTransform;
+    public MenuCtrl menuCtrl;
+
+    bool isOn;      // True: On, False: Off
+
+    private void Start()
     {
-        
+        isOn = false;
+        handleRectTransform.anchoredPosition = new Vector2(-13, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickSwitch()
     {
-        
+        if (isOn == false)
+        {
+            isOn = true;
+            handleRectTransform.anchoredPosition = new Vector2(13, 0);
+        }
+        else
+        {
+            isOn = false;
+            handleRectTransform.anchoredPosition = new Vector2(-13, 0);
+        }
+        menuCtrl.SwitchIsOn(isOn);
     }
 }
--- Assets/Scripts/Menu/MenuCtrl.cs
+++ Assets/Scripts/Menu/MenuCtrl.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,9 +8,15 @@ public class MenuCtrl : MonoBehaviour
 {
     public GameObject settingsPanel;
 
+    AudioSource audioSource;
+    Coroutine settingsCoroutine;
+
     private void Start()
     {
         settingsPanel.SetActive(false);
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
     }
 
     public void StartGame()
@@ -19,13 +26,46 @@ public class MenuCtrl : MonoBehaviour
 
     public void OpenSettings()
     {
-        // 설정창 열기
         settingsPanel.SetActive(true);
+
+        if (settingsCoroutine != null) StopCoroutine(settingsCoroutine);
+        settingsCoroutine = StartCoroutine(openAndCloseSettings(true, 1));
     }
 
     public void CloseSettings()
     {
-        // 설정창 닫기
-        settingsPanel.SetActive(false);
+
+        if (settingsCoroutine != null) StopCoroutine(settingsCoroutine);
+        settingsCoroutine = StartCoroutine(openAndCloseSettings(false, 1, () =>
+        {
+            settingsPanel.SetActive(false);
+        }));
+    }
+
+    // Switch를 위한 함수
+    public void SwitchIsOn(bool isOn)
+    {
+        if (isOn == true)
+            audioSource.Play();
+        else
+            audioSource.Pause();
+    }
+
+    IEnumerator openAndCloseSettings(bool isOpen, float duration, Action callback = null)
+    {
+        CanvasGroup canvasGroup = settingsPanel.GetComponent<CanvasGroup>();
+        float currentTime = 0;
+
+        while (currentTime < duration)
+        {
+            if (isOpen)
+                canvasGroup.alpha = Mathf.Lerp(0, 1, currentTime / duration);
+            else
+                canvasGroup.alpha = Mathf.Lerp(1, 0, currentTime / duration);
+
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        callback.Invoke();
     }
 }
